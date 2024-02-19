ab517b62eced59a4dc55ce5ac3788a92b2dcbae2
Alphocado <raylienardy@gmail.com>
2024-02-19 13:57:32 +0800

    modify php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Git Remote</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati eveniet cumque voluptate debitis quos. Vitae saepe, nulla fuga harum optio repellat quo eum recusandae temporibus non voluptatem consequuntur earum eaque?</p>
+</body>
+
+</html>
